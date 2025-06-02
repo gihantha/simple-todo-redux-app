a00e952f8fb0d799d263674b63dd8794f47513be
gihantha <gihantha01@gmail.com>
2025-06-02 21:01:11 +0530

    Installed prettier and created a UI for task add.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import {useState} from "react";
 
 
 const App =() => {
 
+    const [task, setTask] = useState('');
+
+    const handleInput = (event) => {
+        // console.log(event.target.value)
+
+        setTask(event.target.value);
+    }
+
   return (
-    <>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>    </>
+      <>
+          <div>
+              <label htmlFor="small-input"
+                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task</label>
+              <input type="text" id="small-input" name="task" onChange={handleInput}
+                     className=" w-1/2 m-4 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+              <button type="button"
+                      className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Purple
+                  to Blue
+              </button>
+
+          </div>
+          <div>
+              <h1>{task}</h1>
+          </div>
+      </>
   )
 }
 
